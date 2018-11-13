18e593083d63903466a2c06f9fc4f7376fa64961
Ross Hansen <hansen.ross@live.com.au>
2018-11-14 00:23:58 +0800

    two inital models which need further expansion

--- ARPos/cashregister/models.py
+++ ARPos/cashregister/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Cart(models.Model):
+    ''' Class for defining the Shopping Cart '''
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+class CartItems(models.Model):
+    ''' Class for defining all the products added to the shopping cart '''
+    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
